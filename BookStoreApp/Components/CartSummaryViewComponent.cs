07f9bb1bb6eb8124e34b3cd77a2242c0c171322c
justinbrown11 <justin.j.brown@outlook.com>
2023-03-14 08:52:08 -0600

    finished features

---
+++ BookStoreApp/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using BookStoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+
+        public CartSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
