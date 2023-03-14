80a09c3f4c3e82305e311934310f760480d978ca
justinbrown11 <justin.j.brown@outlook.com>
2023-03-14 09:02:24 -0600

    Added comments

--- BookStoreApp/Components/CartSummaryViewComponent.cs
+++ BookStoreApp/Components/CartSummaryViewComponent.cs
@@ -7,15 +7,19 @@ using System.Threading.Tasks;
 
 namespace BookStoreApp.Components
 {
+    // For the cart view at the top of page
     public class CartSummaryViewComponent : ViewComponent
     {
+        // Declare basket
         private Basket basket;
 
+        // Instantiate basket
         public CartSummaryViewComponent(Basket b)
         {
             basket = b;
         }
-
+        
+        // Return view
         public IViewComponentResult Invoke()
         {
             return View(basket);
--- BookStoreApp/Controllers/PurchaseController.cs
+++ BookStoreApp/Controllers/PurchaseController.cs
@@ -9,9 +9,11 @@ namespace BookStoreApp.Controllers
 {
     public class PurchaseController : Controller
     {
+        // Purchase repo
         private IPurchaseRepository repo { get; set; }
         private Basket basket { get; set; }
 
+        // Instatiation for repo and basket
         public PurchaseController(IPurchaseRepository temp, Basket b)
         {
             repo = temp;
@@ -21,19 +23,22 @@ namespace BookStoreApp.Controllers
         [HttpGet]
         public IActionResult Checkout()
         {
-            return View(new Purchase());
+            return View(new Purchase()); // Return form
         }
 
         [HttpPost]
         public IActionResult Checkout(Purchase purchase)
         {
+            // If there are no basket items, the cart is empty, throw error
             if (basket.Items.Count() == 0)
             {
                 ModelState.AddModelError("", "Sorry your cart is empty!");
             }
 
+            // Check model state
             if (ModelState.IsValid)
             {
+                // Add basket items to purchase lines and clear the current basket
                 purchase.Lines = basket.Items.ToArray();
                 repo.SavePurchase(purchase);
                 basket.ClearBasket();
